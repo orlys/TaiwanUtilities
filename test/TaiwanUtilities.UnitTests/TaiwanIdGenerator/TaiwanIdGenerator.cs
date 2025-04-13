1959596179273fd552ba49b765c8d4b800920e89
Orlys <contact@orlys.ma>
2025-04-13 10:24:31 +0800

    Refactor and add ID validation and generation methods
    Significant refactor and reorganization of the codebase:
    - Removed several validation methods and files.
    - Added `TaiwanIdGenerator.cs` for generating various Taiwanese IDs.
    - Re-added `TaiwanIdValidator.BaNumber.cs` with updated structure.
    - Updated `TaiwanUtilities.csproj` to use wildcard versions for dependencies.
    - Introduced new validation methods in separate files for:
      - Citizen Digital Certificate Numbers
      - Electronic Invoice Donate Codes
      - Electronic Invoice Mobile Barcodes
      - Identity Card Numbers
    - Implemented conditional compilation for regex patterns based on .NET version.
    - Added core matching method `MatchCore` for regex validation.
    - Included commented-out unit tests for ID generation and validation.

---
+++ test/TaiwanUtilities.UnitTests/TaiwanIdGenerator/TaiwanIdGenerator.cs
@@ -0,0 +1,24 @@
+﻿
+//namespace TaiwanUtilities.UnitTests;
+
+//using Xunit;
+
+//using static TaiwanIdValidator;
+
+//[Trait("Category", "Unit")]
+//public partial class TaiwanIdGeneratorTest
+//{
+//    [Fact]
+//    public static void 營利事業統一編號()
+//    {
+//        for (int i = 0; i < 100; i++)
+//        {
+
+//            var actual = TaiwanIdGenerator.GenerateBusinessAdministrationNumber();
+
+//            Assert.True(
+//                condition: IsBusinessAdministrationNumber(actual, applyOldRules: false),
+//                 $"產生的營利事業統一編號 {actual} 不符合規則");
+//        }
+//    }
+//}
