e38ab6f0611954a03885bdcfa50263f7be4c0bf0
Orlys <contact@orlys.ma>
2025-04-14 00:41:46 +0800

    Update RocDateTimeTest with new test case and reordering
    Updated the test method `民國年字串解析` in `RocDateTimeTest`:
    - Added a new test case for input "民國101年02月29日" with expected date "2012/02/29".
    - Moved the `InlineData` attribute for the test case with input "民國貳十參年陸月玖日" and expected date "1934/06/09" to a different position.

--- test/TaiwanUtilities.UnitTests/RocDateTime/RocDateTimeTest.Parsing.cs
+++ test/TaiwanUtilities.UnitTests/RocDateTime/RocDateTimeTest.Parsing.cs
@@ -14,8 +14,9 @@ partial class RocDateTimeTest
 
 	[Theory]
 	[InlineData("民國貳參年陸月玖日", "1934/06/09")]
-	[InlineData("民國貳十參年陸月玖日", "1934/06/09")]
-	public static void 民國年字串解析(string input, string expectedDate)
+    [InlineData("民國貳十參年陸月玖日", "1934/06/09")]
+    [InlineData("民國101年02月29日", "2012/02/29")]
+    public static void 民國年字串解析(string input, string expectedDate)
 	{
 		Assert.Equal(
 			expected: DateTime.Parse(expectedDate), 
